81ce7cab4f37c729549011168243a9fb45500a7c
Sam Macbeth <sam@cliqz.com>
2017-01-04 18:01:44 +0100

    WIP react-native

---
+++ jsengine-android/jsengine/src/main/java/com/cliqz/jsengine/ui/ConversationUIActivity.java
@@ -0,0 +1,15 @@
+package com.cliqz.jsengine.ui;
+
+import com.facebook.react.ReactActivity;
+
+public class ConversationUIActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "ConversationUI";
+    }
+}
